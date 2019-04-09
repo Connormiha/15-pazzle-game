7ab070e552c71d4fb74335dcd678d0218ed1e458
Mihail Bodrov <bodrovmiha@yandex.ru>
2019-04-10 01:37:16 +0300

    Create gh-pages :tada:

---
+++ precache-manifest.925d24220f1f4ccb942fbcb2f9534152.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3cd37b243e332421750",
+    "url": "/static/css/main.a47d0264.chunk.css"
+  },
+  {
+    "revision": "a3cd37b243e332421750",
+    "url": "/static/js/main.a7ea82a6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c822cca656fb4e019796",
+    "url": "/static/js/2.8b03f0dc.chunk.js"
+  },
+  {
+    "revision": "7fa1250af2264da7b24ff01940eed65f",
+    "url": "/index.html"
+  }
+];
